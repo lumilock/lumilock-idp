c0d44166884cbe96d021320a4e879bce7c4bd3ec
Thibaud Perrin <thibaud.perrin@outlook.com>
2022-01-25 14:10:39 +0100

    v1 of LoginCanvas

--- packages/front/src/pages/Login/LoginCanvas/index.jsx
+++ packages/front/src/pages/Login/LoginCanvas/index.jsx
@@ -12,8 +12,8 @@ const COLORS = [
   '#DB7777', // red
   '#141720', // black
 ];
-const COLUMNS = 8; // number of columns
-const GAP = 0; // gap between shapes
+const COLUMNS = 50; // number of columns
+const GAP = 10; // gap between shapes
 const BG = '#FFFFFF'; // background color
 
 function LoginCanvas() {
@@ -27,7 +27,7 @@ function LoginCanvas() {
 
   // draw arc
   const drawArc = useCallback((x, y, color) => {
-    const rayon = Math.max((columnSize / 2) - (GAP / 2), 0);
+    const rayon = Math.max((columnSize - GAP) / 2, 0);
     context.fillStyle = color;
     context.beginPath();
     context.arc(x + rayon, y + rayon, rayon, 0, 2 * Math.PI, false);
@@ -59,18 +59,18 @@ function LoginCanvas() {
   const drawDiamond = useCallback((x, y, color) => {
     context.fillStyle = color;
     // Find center position of the shape
-    const cx = x + ((columnSize) / 2);
-    const cy = y + ((columnSize) / 2);
+    const cx = x + ((columnSize - GAP) / 2);
+    const cy = y + ((columnSize - GAP) / 2);
     // Transform properties
     context.translate(cx, cy); // translate to center of shape
     context.rotate((45 * Math.PI) / 180);
     context.scale(0.71, 0.71);
-    context.fillRect(-((columnSize) / 2), -((columnSize) / 2), columnSize - GAP, columnSize - GAP);
+    context.fillRect(-((columnSize - GAP) / 2), -((columnSize - GAP) / 2), columnSize - GAP, columnSize - GAP);
     const isHoled = rand(0, 2);
     if (isHoled === 1) {
       const newSize = (columnSize - GAP) / 3;
       context.fillStyle = BG;
-      context.fillRect(-((newSize + GAP) / 2), -((newSize + GAP) / 2), newSize, newSize);
+      context.fillRect(-((newSize) / 2), -((newSize) / 2), newSize, newSize);
     }
     // Reset transform
     context.setTransform(1, 0, 0, 1, 0, 0);
@@ -111,14 +111,87 @@ function LoginCanvas() {
     context.fill();
   }, [columnSize, context, rand]);
 
+  const drawRoundedCorner = useCallback((x, y, color) => {
+    // Find center position of the shape
+    const cx = x + ((columnSize - GAP) / 2);
+    const cy = y + ((columnSize - GAP) / 2);
+    context.fillStyle = color;
+    context.beginPath();
+    // Transform properties
+    context.translate(cx, cy); // translate to center of shape
+    const rotation = rand(0, 3);
+    switch (rotation) {
+      case 0: { // Bottom left
+        context.rotate((-90 * Math.PI) / 180);
+        break;
+      }
+      case 1: { // Top right
+        context.rotate((90 * Math.PI) / 180);
+        break;
+      }
+      case 2: { // Top left
+        context.rotate((0 * Math.PI) / 180);
+        break;
+      }
+      default: { // Bottom left
+        context.rotate((180 * Math.PI) / 180);
+        break;
+      }
+    }
+    context.arc(-((columnSize - GAP) / 2), -((columnSize - GAP) / 2), Math.max(columnSize - GAP, 0), 0, Math.PI / 2, false);
+    context.lineTo(-((columnSize - GAP) / 2), -((columnSize - GAP) / 2));
+    context.fill();
+    context.closePath();
+    // Reset transform
+    context.setTransform(1, 0, 0, 1, 0, 0);
+  }, [columnSize, context, rand]);
+
+  const drawRoundedSquare = useCallback((x, y, color) => {
+    const rayon = Math.max((columnSize - GAP) / 2, 0);
+    // Find center position of the shape
+    const cx = x + rayon;
+    const cy = y + rayon;
+    context.fillStyle = color;
+    context.beginPath();
+    // Transform properties
+    context.translate(cx, cy); // translate to center of shape
+    const rotation = rand(0, 3);
+    switch (rotation) {
+      case 0: { // Bottom left
+        context.rotate((-90 * Math.PI) / 180);
+        break;
+      }
+      case 1: { // Top right
+        context.rotate((90 * Math.PI) / 180);
+        break;
+      }
+      case 2: { // Top left
+        context.rotate((0 * Math.PI) / 180);
+        break;
+      }
+      default: { // Bottom left
+        context.rotate((180 * Math.PI) / 180);
+        break;
+      }
+    }
+    context.arc(0, 0, rayon, 0, Math.PI, false);
+    context.lineTo(-rayon, -rayon);
+    context.lineTo(rayon, -rayon);
+    context.fill();
+    context.closePath();
+    // Reset transform
+    context.setTransform(1, 0, 0, 1, 0, 0);
+  }, [columnSize, context, rand]);
+
   // All possible shapes
   const shapes = useMemo(() => [
     drawArc, // circle
     drawSquare, // square
     drawTriangle, // triangle
     drawDiamond, // diamond
-    // (x, y, color) => console.log('3.', x, y, color), // rounded square
-  ], [drawArc, drawSquare, drawDiamond, drawTriangle]);
+    drawRoundedCorner, // rounded corner
+    drawRoundedSquare, // rounded square
+  ], [drawArc, drawSquare, drawDiamond, drawTriangle, drawRoundedCorner, drawRoundedSquare]);
 
   const draw = useCallback(
     () => {
@@ -128,12 +201,17 @@ function LoginCanvas() {
         Array.from({ length: ROWS }, (_, k) => k).forEach((rIndex) => {
           // Peak a number between 0 and COLORS length - 1 to peak a random color
           const rColorIndex = rand(0, (COLORS.length - 1));
-          context.fillStyle = COLORS[rColorIndex];
 
+          // Draw the shape
+          // context.fillStyle = `${COLORS[rColorIndex]}33`;
+          // context.fillRect(cIndex * columnSize + GAP / 2, rIndex * columnSize + GAP / 2, columnSize - GAP, columnSize - GAP);
+
+          context.fillStyle = COLORS[rColorIndex];
           const rShapeIndex = rand(0, (shapes.length - 1));
           shapes?.[rShapeIndex](cIndex * columnSize + GAP / 2, rIndex * columnSize + GAP / 2, COLORS[rColorIndex]);
-          // Draw the shape
-          // context.fillRect(cIndex * columnSize + GAP / 2, rIndex * columnSize + GAP / 2, columnSize - GAP, columnSize - GAP);
+
+          // context.rect((cIndex * columnSize) + (GAP / 2), (rIndex * columnSize) + (GAP / 2), columnSize - GAP, columnSize - GAP);
+          // context.stroke();
         });
       });
     }, [ROWS, columnSize, context, rand, shapes],
