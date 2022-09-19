e72dfb7ec9b7cf1c62f8b2acabd70dcfeb80bfa3
Thibaud Perrin <thibaud.perrin@outlook.com>
2022-09-19 20:44:38 +0200

    starting reset form

--- packages/front/src/components/Cells/ResetPassword/index.jsx
+++ packages/front/src/components/Cells/ResetPassword/index.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 function ResetPassword() {
   return (
-    <div>ResetPassword</div>
+    <div>
+      <p>Mot de passe oublié ?</p>
+      <p>Enter the identity associated with your account and we&apos;ll send you a link to reset your password.</p>
+      <p>Identity:</p>
+      <p>Send</p>
+      <p>Don&apos;t have an account ? Contact an administrator</p>
+    </div>
   );
 }
 
